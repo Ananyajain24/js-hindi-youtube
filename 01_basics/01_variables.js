29f8aae44586074bda9e7c92ec124c5c5bf3d30e
Ananyajain24 <149472137+Ananyajain24@users.noreply.github.com>
2024-06-24 07:38:37 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,7 @@
+const accountId = 144553
+let accountEmail = "ananya@gmail.com"
+var accountPassword ="12345"
+accountCity = "Jaipur"
+//accountId = 2 //not allowed 
+console.log(accountId);
+console.table([accountId, accountEmail, accountCity]) 
---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,14 @@
+"use strict"; //treat all js code as newer version 
+// alert(3+3)
+let state;
+let isLoggedIn = fasle;
+// number => 2 to power 53
+//bigint
+//string
+//boolean 
+//null
+//undefined => 
+//symbol => unique
+ console.log(typeof "Ananya"); //string
+ console.log(typeof null); //Its type is object 
+ console.log(typeof undefined); //its type is undefined 
